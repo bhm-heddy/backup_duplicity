234006348f9684ea0ad5a6eb0c8e2b7b715ea58d
heddy <bhm.heddy@protonmail.com>
2020-09-21 16:53:01 +0000

    add list-files.sh recover.sh

--- script/list-files.sh
+++ script/list-files.sh
@@ -13,32 +13,32 @@ source cfg.sh
 
 CONSIGNE="Entrer :\n- Une date specifique\n- Vide ou 0 pour le backup le plus récent\n- 1 pour afficher les details des backup\n   ->: "
 
-FORMAT='\n[FORMAT] :\nThe acceptible time strings are intervals (like "3D64s") \nw3-datetime strings, like "2002-04-26T04:22:01-07:00" (strings like "2002-04-26T04:22:01" are also acceptable - duplicity will use the current time zone)\nor ordinary dates like 2/4/1997 or 2001-04-23 (various combinations are acceptable, but the month always precedes the day)\n\n'
+TIME_FORMAT='\n[TIME FORMAT] :\nThe acceptible time strings are intervals (like "3D64s") \nw3-datetime strings, like "2002-04-26T04:22:01-07:00" (strings like "2002-04-26T04:22:01" are also acceptable - duplicity will use the current time zone)\nor ordinary dates like 2/4/1997 or 2001-04-23 (various combinations are acceptable, but the month always precedes the day)\n\n'
 
 
 
-echo -e $FORMAT
+echo -e $TIME_FORMAT
 
 echo -ne "$CONSIGNE"
 
-read time
+read TIME
 
-while [ "$time"  == "1" ]
+while [ "$TIME"  == "1" ]
 do
 	bash $BACKUP_PATH/script/collection-status.sh
 	echo -ne "\n\n$CONSIGNE"
-	read time
+	read TIME
 done
 
-if [ -z $time ]
+if [ -z $TIME ]
 then
-	time=0
+	TIME=0
 fi
 
 
 
 duplicity \
-	list-current-files -t $time \
+	list-current-files -t $TIME \
 	--encrypt-key $ENC_KEY \
 	--sign-key $SIG_KEY \
 	$SCW_BUCKET
