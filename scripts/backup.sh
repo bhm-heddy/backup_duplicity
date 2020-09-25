7f662b2a51f5bd571ec548851caf39d64c0c8189
heddy <bhm.heddy@protonmail.com>
2020-09-25 14:59:20 +0000

    protection nextcloud.occ

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,32 +1,38 @@
 #!/bin/bash
 
+# Source si des fichiers de configuration sont passés en argument
 
 for av in $@; do
 	if [ -r $av ]; then
 		source $av
 	else
-		>&2 echo "Bad file: $av"
+		>&2 echo "Fichier inexistant ou de permissions: $av"
 		exit 1
 	fi
 done
 
 
 
-export SRC_PATH=${SRC_PATH:-/var/lib/backup}
-export LOG_PATH=${LOG_PATH:-/var/log}
-export REMOVE_BACKUP_TIME=${REMOVE_BACKUP_TIME:-6M}
-export FULL_BACKUP_TIME=${FULL_BACKUP_TIME:-1M}
+# Initialise ces variables si elles n'existent pas
+
+SRC_PATH=${SRC_PATH:-/var/lib/backup}
+LOG_PATH=${LOG_PATH:-/var/log}
+REMOVE_BACKUP_TIME=${REMOVE_BACKUP_TIME:-6M}
+FULL_BACKUP_TIME=${FULL_BACKUP_TIME:-1M}
 
 
+
+# Vérifie l'existence de ces variables
 
 if [ -z $ENC_KEY ] || [ -z $SIG_KEY ] \
 	|| [ -z $PASSPHRASE ] || [ -z $SIGN_PASSPHRASE ] \
 	|| [ -z $AWS_ACCESS_KEY_ID ] || [ -z $AWS_SECRET_ACCESS_KEY ] \
 	|| [ -z $SCW_BUCKET ]
 then
-	>&2 echo "message d erreur + info"
+	>&2 echo "Une ou des variables n'ont pas été initialisées. Voir README.ME"
 	exit 1
 fi
+
 
 
 DATE=`date +%Y-%m`
@@ -34,16 +40,23 @@ DAY=`date +%d`
 HOUR=`date +%H:%M:%S`
 
 
-## Maintence mode. Locks the sessions of logged-in users and prevents new logins in order to prevent inconsistencies of your data
+
+## Active le mode maintenance de nextcloud
 
 sudo nextcloud.occ maintenance:mode --on
 
 
+if [ $? -ne 0 ]; then
+	>&2 echo "[BACKUP ERROR]   L'activation du monde maintenance a échouée"
+	exit 1
+fi
 
-## First call duplicity, delete all backup sets older than the REMOVE_BACK_TIME
 
 echo -e "\t\t[BACKUP]\t$DATE-$DAY\t$HOUR\n" >>$LOG_PATH/backup_$DATE.log
 echo -e "\t--- Removing old backups\n" >>$LOG_PATH/backup_$DATE.log
+
+
+## Suppression des plus vieux backup
 
 duplicity \
 	remove-older-than $REMOVE_BACKUP_TIME \
@@ -54,11 +67,15 @@ duplicity \
 	>>$LOG_PATH/backup_$DATE.log \
 	2>&1
 
+if [ $? -ne 0 ]; then
+	>&2 echo "[BACKUP ERROR]  La suppression des anciens backup a échouée."
+fi
 
-
-## Second call, encrypt incremental backup
 
 echo -e "\t--- Creating and uploading backup\n" >>$LOG_PATH/backup_$DATE.log
+
+
+## Sauvegarde
 
 duplicity \
 	--full-if-older-than $FULL_BACKUP_TIME \
@@ -72,24 +89,18 @@ duplicity \
 	>>$LOG_PATH/backup_$DATE.log \
 	2>&1
 
+if [ $? -ne 0 ]; then
+	>&2 echo "[BACKUP ERROR]  La sauvegarde a échouée."
+fi
 
 
-## Disable maintenance mode
+## Desactive le mode maintenance
 
 sudo nextcloud.occ maintenance:mode --off
 
+if [ $? -ne 0 ]; then
+	>&2 echo "[BACKUP ERROR]   La désactivation du monde maintenance a échouée"
+	exit 1
+fi
 
-
-
-unset enc_key
-unset sig_key
-unset passphrase
-unset sign_passphrase
-unset aws_access_key_id
-unset aws_secret_access_key
-unset scw_bucket
-unset repo_path
-unset src_path
-unset log_path
-unset remove_backup_time
-unset full_backup_time
+exit 0
