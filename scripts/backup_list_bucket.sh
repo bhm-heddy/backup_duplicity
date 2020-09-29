e5eff71b2c5679271632109e93fb11d7db1f2651
bhm-heddy <68072640+bhm-heddy@users.noreply.github.com>
2020-09-29 10:33:52 +0000

    Update backup_list_bucket.sh

--- scripts/backup_list_bucket.sh
+++ scripts/backup_list_bucket.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
 
+# Source si des fichiers de configuration sont passés en argument
+
 for av in $@; do
 	if [ -r $av ]; then
 		source $av
 	else
-		>&2 echo "Bad file: $av"
+		>&2 echo "Fichier inexistant ou permissions insuffisantes: $av"
 		exit 1
 	fi
 done
+
+
+# Vérifie l'existence de ces variables
 
 if [ -z $ENC_KEY ] || [ -z $SIG_KEY ] \
 	|| [ -z $PASSPHRASE ] || [ -z $SIGN_PASSPHRASE ] \
 	|| [ -z $AWS_ACCESS_KEY_ID ] || [ -z $AWS_SECRET_ACCESS_KEY ] \
 	|| [ -z $SCW_BUCKET ]
 then
-	>&2 echo "message d erreur + info"
+	>&2 echo "Une ou des variables n'ont pas été initialisées. Voir README.ME"
 	exit 1
 fi
 
@@ -25,11 +30,5 @@ duplicity \
 	--encrypt-key $ENC_KEY \
 	--sign-key $SIG_KEY \
 	$SCW_BUCKET
-
-unset ENC_KEY
-unset SIG_KEY
-unset PASSPHRASE
-unset SIGN_PASSPHRASE
-unset AWS_ACCESS_KEY_ID
-unset AWS_SECRET_ACCESS_KEY
-unset SCW_BUCKET
+	
+exit $?
